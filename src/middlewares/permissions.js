26d215bb563fe4200b2f9cf67c364c9cdafd594f
ogbon <onethinneedful@gmail.com>
2021-08-05 05:29:26 +0100

    add permissions.js and validators.js files

---
+++ src/middlewares/permissions.js
@@ -0,0 +1,37 @@
+import {decodeJWTToken} from '../helpers/authTools'
+import {isAdmin} from '../helpers/tools'
+
+import {UNAUTHORIZED} from '../constants/statusCodes'
+
+
+const permissions = {
+  isAdmin: async (req, res, next) => {
+    try {
+      const user = decodeJWTToken(req.headers.authorization)
+
+      if (user && await isAdmin(user.role_id)) {
+        req.decoded = user
+
+        next()
+      } else {
+        res.status(UNAUTHORIZED).send({data: null, message: 'Current user is unauthorized', success: false})
+      }
+    } catch(err) { res.status(UNAUTHORIZED).send({data: null, message: 'Current user is unauthorized', success: false}) }
+  },
+  isAuthenticated: async (req, res, next) => {
+    try {
+      const user = decodeJWTToken(req.headers.authorization)
+
+      if (user) {
+        req.decoded = user
+
+        next()
+      } else {
+        res.status(UNAUTHORIZED).send({data: null, message: 'Please login to your account.', success: false})
+      }
+    } catch (err) { res.status(UNAUTHORIZED).send({data: null, message: 'Please login to your account.', success: false}) }
+  },
+  
+}
+
+export default permissions
