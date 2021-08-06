7577494051546b3534892b51313cc71dee90f214
ogbon <onethinneedful@gmail.com>
2021-08-06 18:29:56 +0100

    add changes to .sequelizerc, models/index.js and .travis.yml files

--- db/models/index.js
+++ db/models/index.js
@@ -3,6 +3,9 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+
+require('dotenv').config()
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
